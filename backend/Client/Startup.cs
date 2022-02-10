ca892ccc5c62f247a3a05eb2288d33ba0203bd6b
hoang0502thao <89056218+hoang0502thao@users.noreply.github.com>
2022-02-11 06:45:47 +0700

    code trong Area

--- backend/Client/Startup.cs
+++ backend/Client/Startup.cs
@@ -25,7 +25,8 @@ namespace Client
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllersWithViews();
+           
+            services.AddControllersWithViews().AddRazorRuntimeCompilation();
             services.AddSession();
             services.AddDbContext<InsuranceDBContext>(options =>
                     options.UseSqlServer(Configuration.GetConnectionString("Db")));
