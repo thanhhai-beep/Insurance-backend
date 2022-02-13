4a85a6469ca94859f4b551789e95901b1a7be9ac
hoang0502thao <89056218+hoang0502thao@users.noreply.github.com>
2022-02-13 18:04:56 +0700

    employee Pages

---
+++ backend/Client/Areas/Employee/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Client.Areas.Employee.Controllers
+{
+    [Area("Employee")]
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- backend/Client/Startup.cs
+++ backend/Client/Startup.cs
@@ -63,6 +63,14 @@ namespace Client
                  pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
                );
                 endpoints.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+                endpoints.MapControllerRoute(
+            name: "areas",
+            pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}"
+          );
+                endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
             });
