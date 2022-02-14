a5212b60187d796d77787e22056c23423f9c84dd
Giang <luncon0090@gmail.com>
2022-02-14 11:30:58 +0700

    controller api

---
+++ backend/backend/Controllers/FeedbackController.cs
@@ -0,0 +1,110 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FeedbackController : ControllerBase
+    {
+        private readonly InsuranceDBContext _context;
+
+        public FeedbackController(InsuranceDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Feedback>>> GetFeedback()
+        {
+            return await _context.Feedbacks.ToListAsync();
+        }
+
+        // GET: api/Employees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Feedback>> GetFeedbacks(int id)
+        {
+            var feedback = await _context.Feedbacks.FindAsync(id);
+
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+            return feedback;
+        }
+
+        //[HttpGet("User/{id}")]
+        //public async Task<ActionResult<IEnumerable<Feedback>>> GetFeedBackOfEmp(int id)
+        //{
+        //    return await _context.Feedbacks
+        //    .Where(fb => !fb.Title && fb.Id == id)
+        //    .ToListAsync();
+        //}
+
+        [HttpPost]
+        public async Task<ActionResult<Feedback>> PostFeedback(Feedback feedback)
+        {
+            _context.Feedbacks.Add(feedback);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetFeedback", new { id = feedback.Id }, feedback);
+        }
+
+
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutFeedback(int id, Feedback feedback)
+        {
+            if (id != feedback.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(feedback).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!FeedbackExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<Feedback>> DeleteFeedback(int id)
+        {
+            var feedback = await _context.Feedbacks.FindAsync(id);
+            if (feedback == null)
+            {
+                return NotFound();
+            }
+
+            _context.Feedbacks.Remove(feedback);
+            await _context.SaveChangesAsync();
+
+            return feedback;
+        }
+
+        private bool FeedbackExists(int id)
+        {
+            return _context.Feedbacks.Any(e => e.Id == id);
+        }
+
+    }
+}
---
+++ backend/backend/Controllers/HospitalController.cs
@@ -0,0 +1,126 @@
+﻿using backend.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HospitalController : ControllerBase
+    {
+        private readonly InsuranceDBContext _context;
+
+        public HospitalController(InsuranceDBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<HospitalInfo>>> GetHospital()
+        {
+            return await _context.HospitalInfos.ToListAsync();
+        }
+
+        // GET: api/Employees/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<HospitalInfo>> GetHospitals(int id)
+        {
+            var hos = await _context.HospitalInfos.FindAsync(id);
+
+            if (hos == null)
+            {
+                return NotFound();
+            }
+            return hos;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<HospitalInfo>> PostHospital(HospitalInfo hospital)
+        {
+            _context.HospitalInfos.Add(hospital);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetHospital", new { id = hospital.Id }, hospital);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutHospital(int id, HospitalInfo hospital)
+        {
+            if (id != hospital.Id)
+            {
+                return BadRequest();
+            }
+
+            //_context.Entry(hospital).State = EntityState.Modified;
+            _context.Update(hospital);
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!HospitalExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return Ok(hospital);
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<HospitalInfo>> DeleteHospital(int id)
+        {
+            var hos = await _context.HospitalInfos.FindAsync(id);
+            if (hos == null)
+            {
+                return NotFound();
+            }
+            _context.HospitalInfos.Remove(hos);
+            await _context.SaveChangesAsync();
+
+            return hos;
+        }
+
+        private bool HospitalExists(int id)
+        {
+            return _context.HospitalInfos.Any(e => e.Id == id);
+        }
+
+
+
+        //ko search đc
+        [HttpPost("Search")]
+        public async Task<ActionResult<IEnumerable<HospitalInfo>>> Search([FromForm] string HospitalName, [FromForm] string Phone, [FromForm] string Address)
+        {
+            var query = _context.HospitalInfos.Include(ar => ar.Address).AsQueryable();
+
+            if (HospitalName != null)
+            {
+                query = query.Where(h => h.HospitalName.Contains(HospitalName));
+            }
+            if (Address != null)
+            {
+                query = query.Where(a => a.Address.Contains(Address));
+            }
+
+            if (Phone != null)
+            {
+                query = query.Where(p => p.Phone.Contains(Phone));
+            }
+            return await query.ToListAsync();
+        }
+
+    }
+}
