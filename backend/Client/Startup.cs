a1782133fdff02647f24b1f76c36294326e1158a
Thanhhai-beep <dohai30112002@gmail.com>
2022-02-15 10:27:56 +0700

    fontend

--- backend/Client/Startup.cs
+++ backend/Client/Startup.cs
@@ -72,7 +72,7 @@ namespace Client
           );
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Login}/{action=Login}/{id?}");
             });
         }
     }
